2572a2d3434113cc9309b68463ebe45d2e5d4269
resteve <resteve@zikzakmedia.com>
2013-12-24 10:00:55 +0100

    Update party vals from csv data after on change party

--- csv_import.py
+++ csv_import.py
@@ -31,7 +31,9 @@ class CSVArchive:
                 Party = pool.get('party.party')
                 party = Party(party)
                 model.party = party
-                values.update(Purchase(**values).on_change_party())
+                vals = Purchase(**values).on_change_party()
+                vals.update(values)
+                values = vals.copy()
 
         if model_name == 'purchase.line':
             if values.get('product') and values.get('quantity'):
