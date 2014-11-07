56d9a32a165d04c7c58616eedb6f8c0d5b3291a0
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:21:37 +0100

    Cleanup tests

--- tests/test_csv_purchase.py
+++ tests/test_csv_purchase.py
@@ -7,24 +7,16 @@ import sys
 import trytond.tests.test_tryton
 import unittest
 
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class CSVPurchaseTestCase(unittest.TestCase):
-    '''
-    Test CSV Purchase module.
-    '''
+    'Test CSV Purchase module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('csv_purchase')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
